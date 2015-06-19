fd6798518f2ed082c43ca4ff6a2fd77473545bba
Darko Kotoski <wbountyhunter@hotmail.com>
2015-06-19 18:17:11 +0200

    initial commit

---
+++ OMerce.Core/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMerce.Core
+{
+    public abstract class BaseEntity
+    {
+        public int ID { get; set; }
+        public DateTime DateCreated { get; private set; }
+        public bool IsDeleted { get; set; }
+
+        public BaseEntity()
+        {
+            DateCreated = DateTime.Now;
+        }
+    }
+}
